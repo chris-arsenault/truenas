b0505d3e9aaa04a736d091ebbe4bf8bb0d8d65f9
Chris Arsenault <carsena2@gmail.com>
2026-04-09 19:57:39 -0700

    fix: update sonarqube_domain to sonar.services.ahara.io
    Cognito callback URL was still using sonar.ahara.io but the reverse
    proxy now serves at sonar.services.ahara.io, causing redirect_mismatch.

--- infrastructure/terraform/locals.tf
+++ infrastructure/terraform/locals.tf
@@ -4,6 +4,6 @@ data "aws_region" "current" {}
 locals {
   prefix           = "nas-sonarqube"
   domain_name      = "ahara.io"
-  sonarqube_domain = "sonar.${local.domain_name}"
+  sonarqube_domain = "sonar.services.${local.domain_name}"
   ssm_prefix       = "/ahara/sonarqube"
 }
